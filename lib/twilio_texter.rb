00a29971120c05686d089d2a1aca1cf9a1b7ee71
vicfriedman <victoria.m.friedman@gmail.com>
2014-05-22 13:02:22 -0400

    changed user for testing

--- lib/twilio_texter.rb
+++ lib/twilio_texter.rb
@@ -5,7 +5,7 @@ def text_me_instagram
   account_sid = ENV['TWILIO-SID']
   auth_token = ENV['TWILIO-TOKEN']
   @client = Twilio::REST::Client.new account_sid, auth_token
-  @instagram_scraper = InstagramScraper.new(ENV['INSTAGRAM_AUTH'])
+  @instagram_scraper = InstagramScraper.new(ENV['INSTAGRAM_AUTH_TOKEN'])
   user_id = @instagram_scraper.get_user_id
   all_pics = @instagram_scraper.get_pics(user_id.id)
   most_recent_pic = all_pics.first
