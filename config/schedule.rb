1304b0833a08df7fa09d17f6c91d589c729523bd
vicfriedman <victoria.m.friedman@gmail.com>
2014-06-02 10:12:14 -0400

    testing twilio and heroku

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,8 +1,8 @@
-set :whenever_command, "bundle exec whenever"
-set :environment, "production"
-set :cron_log, "./log/cron_log.log"
+# set :whenever_command, "bundle exec whenever"
+# set :environment, "production"
+# set :cron_log, "./log/cron_log.log"
 
-every 1.minute do
-  rake :check_most_recent_photo
-end
+# every 1.minute do
+#   rake :check_most_recent_photo
+# end
 
